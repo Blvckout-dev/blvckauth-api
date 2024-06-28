9ab10a528208bd2a323fd8ce04b405f3600ebd82
Maximilian <Maximilian0712@outlook.de>
2024-06-28 18:41:38 +0200

    Added authentication and database support

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,22 +1,76 @@
 using Bl4ckout.MyMasternode.Auth.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace Bl4ckout.MyMasternode.Auth.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class AuthController(ILogger<AuthController> logger, IJwtTokenService jwtTokenService) : ControllerBase
+public class AuthController(ILogger<AuthController> logger, IJwtTokenService jwtTokenService, Database.MyMasternodeAuthDbContext myMasternodeAuthDbContext) : ControllerBase
 {
     private readonly ILogger<AuthController> _logger = logger;
     private readonly IJwtTokenService _jwtTokenService = jwtTokenService;
+    private readonly Database.MyMasternodeAuthDbContext _myMasternodeAuthDbContext = myMasternodeAuthDbContext;
 
-    [HttpPost]
+    [AllowAnonymous]
+    [HttpPost("Login")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     public IActionResult Login([FromBody] Models.Login login)
     {
-        Models.AuthenticationToken? authToken = _jwtTokenService.GenerateToken(login);
+        if (string.IsNullOrWhiteSpace(login.Username) ||
+            string.IsNullOrWhiteSpace(login.Password)
+        )
+            return BadRequest();
 
-        return authToken is null ? Unauthorized() : Ok(authToken);
+        _logger.LogInformation("Token requested for user: {username}", login.Username);
+
+        // Check if user exists
+        Database.Models.User? user = _myMasternodeAuthDbContext.Users
+            .AsNoTracking()
+            .Include(user => user.Role)
+            .Include(user => user.Scopes)
+            .FirstOrDefault(u => u.Username == login.Username);
+
+        if (user is null)
+            return Unauthorized();
+
+        if (string.IsNullOrWhiteSpace(user.Password))
+            return Problem();
+
+        // Verify password
+        PasswordHasher<Database.Models.User> pwh = new(
+            Options.Create(
+                new PasswordHasherOptions()
+                {
+                    CompatibilityMode = PasswordHasherCompatibilityMode.IdentityV3, // V3 uses PBKDF2 with HMAC-SHA256, 128-bit salt, 256-bit subkey, 10000 iterations.
+                    IterationCount = 600000 // Increasing to 600k iterations, recommended by OWASP
+                }
+            )
+        );
+
+        switch (pwh.VerifyHashedPassword(user, user.Password, login.Password))
+        {
+            case PasswordVerificationResult.Failed:
+            return Unauthorized();
+            case PasswordVerificationResult.Success:
+            _logger.LogInformation("User: {username} authenticated successfully", user.Username);
+            break;
+            case PasswordVerificationResult.SuccessRehashNeeded:
+            _logger.LogInformation("User: {username} authenticated successfully, however the password was encoded using a deprecated algorithm and should be rehashed and updated.", user.Username);
+            // ToDo: Implement RehashUserPasswordToLatestVersion()
+            break;
+        }
+        
+        Models.AuthenticationToken authTokenResult = _jwtTokenService.GenerateToken(
+            user.Username,
+            user.Role?.Name,
+            user.Scopes?.Select(s => s.Name)
+        );
+
+        return authTokenResult.Success ? Ok(authTokenResult.Token) : Unauthorized(authTokenResult.ErrorMsg);
     }
 }
