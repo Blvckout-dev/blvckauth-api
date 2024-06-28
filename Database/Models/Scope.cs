cecb5a742b7b206239d8682211c8c17e567f4a18
Maximilian <Maximilian0712@outlook.de>
2024-06-28 18:38:14 +0200

    Override Scope equals to check for id and name

--- Database/Models/Scope.cs
+++ Database/Models/Scope.cs
@@ -3,7 +3,20 @@ namespace Bl4ckout.MyMasternode.Auth.Database.Models;
 public class Scope
 {
     public int Id { get; set; }
-    public string? Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     
     public ICollection<User>? Users { get; set; }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is Scope other)
+            return Id == other.Id && Name == other.Name;
+
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id, Name);
+    }
 }
