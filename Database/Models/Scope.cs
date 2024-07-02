ed8a362cfab91b224a783108850533d10c51051f
maximilianbauer <Maximilian0712@outlook.de>
2024-07-02 15:25:36 +0200

    Removed nullable from required db entity model properties

--- Database/Models/Scope.cs
+++ Database/Models/Scope.cs
@@ -3,7 +3,7 @@ namespace Bl4ckout.MyMasternode.Auth.Database.Models;
 public class Scope
 {
     public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
+    public string Name { get; set; } = null!;
     
     public ICollection<User>? Users { get; set; }
 
--- Database/Models/User.cs
+++ Database/Models/User.cs
@@ -6,8 +6,8 @@ namespace Bl4ckout.MyMasternode.Auth.Database.Models;
 public class User
 {
     public int Id { get; set; }
-    public string? Username { get; set; }
-    public string? Password { get; set; }
+    public string Username { get; set; } = null!;
+    public string Password { get; set; } = null!;
     public int RoleId { get; set; } = 1;
     public Role? Role { get; set; }
     public ICollection<Scope>? Scopes { get; set; }
