8ab53641e15b35a74c68fcde0aaa42514332e376
maximilianbauer <Maximilian0712@outlook.de>
2024-08-07 11:34:39 +0200

    Added additional maps for automapper

--- AutoMapper/MappingProfile.cs
+++ AutoMapper/MappingProfile.cs
@@ -6,6 +6,8 @@ public class MappingProfile : Profile
 {
     public MappingProfile()
     {
+        CreateMap<Database.Models.User, DataModels.Auth.V1.DTOs.Users.UserDto>().ReverseMap();
+        CreateMap<Database.Models.User, DataModels.Auth.V1.DTOs.Users.UserCreateDto>().ReverseMap();
         CreateMap<Database.Models.User, DataModels.Auth.V1.DTOs.Users.UserUpdateDto>().ReverseMap();
     }
 }
