0529abca83936b8746dcf416d5c55d031a1bc280
maximilianbauer <Maximilian0712@outlook.de>
2024-08-13 19:39:30 +0200

    Added mappings between role, scope and two more user dtos

--- AutoMapper/MappingProfile.cs
+++ AutoMapper/MappingProfile.cs
@@ -6,8 +6,23 @@ public class MappingProfile : Profile
 {
     public MappingProfile()
     {
-        CreateMap<Database.Models.User, DataModels.Auth.V1.DTOs.Users.UserDto>().ReverseMap();
+        CreateMap<Database.Models.User, DataModels.Auth.V1.DTOs.Users.UserDto>()
+            .ForMember(
+                dest => dest.ScopeIds,
+                opt => opt.MapFrom(
+                    src => src.Scopes != null ?
+                    src.Scopes.Select(s => s.Id) :
+                    Enumerable.Empty<int>()
+                )
+            )
+            .ReverseMap();
+        CreateMap<Database.Models.User, DataModels.Auth.V1.DTOs.Users.UserMinimalDto>().ReverseMap();
+        CreateMap<Database.Models.User, DataModels.Auth.V1.DTOs.Users.UserDetailDto>().ReverseMap();
         CreateMap<Database.Models.User, DataModels.Auth.V1.DTOs.Users.UserCreateDto>().ReverseMap();
         CreateMap<Database.Models.User, DataModels.Auth.V1.DTOs.Users.UserUpdateDto>().ReverseMap();
+
+        CreateMap<Database.Models.Role, DataModels.Auth.V1.DTOs.Roles.RoleDto>().ReverseMap();
+
+        CreateMap<Database.Models.Scope, DataModels.Auth.V1.DTOs.Scopes.ScopeDto>().ReverseMap();
     }
 }
