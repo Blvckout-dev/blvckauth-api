6ddd9a2e89e91927c430350c3b9eff9024e86f33
maximilianbauer <Maximilian0712@outlook.de>
2024-08-23 15:53:48 +0200

    Renamed JwtSettings to Jwt in config and added Audience as well as Issuer to the configuration

--- Settings/JwtSettings.cs
+++ Settings/JwtSettings.cs
@@ -5,6 +5,12 @@ namespace Bl4ckout.MyMasternode.Auth.Settings;
 public class JwtSettings 
 {
     public const string SECTION = "Jwt";
+    
+    [Required(AllowEmptyStrings = false)]
+    public string Audience { get; set; } = string.Empty;
+
+    [Required(AllowEmptyStrings = false)]
+    public string Issuer { get; set; } = string.Empty;
 
     [Required(AllowEmptyStrings = false)]
     public string Key { get; set; } = string.Empty;
