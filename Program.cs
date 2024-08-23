8f7c1195b9df603b7b5e1915c1219aa141eb158b
maximilianbauer <Maximilian0712@outlook.de>
2024-08-23 16:23:51 +0200

    Added catch for (validation) exceptions on application start and replace Bearer string with const

--- Program.cs
+++ Program.cs
@@ -49,21 +49,24 @@ class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen(c => {
-            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+            c.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme()
             {
                 Name = "Authorization",
                 Type = SecuritySchemeType.ApiKey,
-                Scheme = "Bearer",
+                Scheme = JwtBearerDefaults.AuthenticationScheme,
                 BearerFormat = "JWT",
                 In = ParameterLocation.Header,
-                Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+                Description = 
+                    $"JWT Authorization header using the {JwtBearerDefaults.AuthenticationScheme} scheme.{Environment.NewLine}" +
+                    $"Enter '{JwtBearerDefaults.AuthenticationScheme}' [space] and then your token in the text input below.{Environment.NewLine}" +
+                    $"Example: \"{JwtBearerDefaults.AuthenticationScheme} eyJhbGciOiJIUzI1N...\""
             });
             c.AddSecurityRequirement(new OpenApiSecurityRequirement {
                 {
                     new OpenApiSecurityScheme {
                         Reference = new OpenApiReference {
                             Type = ReferenceType.SecurityScheme,
-                            Id = "Bearer"
+                            Id = JwtBearerDefaults.AuthenticationScheme
                         }
                     },
                     Array.Empty<string>()
@@ -120,34 +123,65 @@ class Program
         builder.Services.AddSingleton<IJwtTokenService, JwtTokenService>();
         builder.Services.AddSingleton<IConfigureOptions<JwtBearerOptions>, ConfigureJwtBearerOptions>();
 
-        var app = builder.Build();
-
-        using (var scope = app.Services.CreateScope())
+        try
         {
-            var services = scope.ServiceProvider;
-            var context = services.GetRequiredService<MyMasternodeAuthDbContext>();
-            var databaseSettings = services.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+            var app = builder.Build();
 
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<MyMasternodeAuthDbContext>();
+                var databaseSettings = services.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+
+                if (app.Environment.IsDevelopment())
+                    if (databaseSettings?.SeedData ?? true)
+                        DbInitializer.Initialize(context);
+                
+                // Check for admin user
+                AddOrUpdateAdminUser(context, builder.Configuration.GetSection(AdminSettings.SECTION).Get<AdminSettings>());
+            }
+
+            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
-                if (databaseSettings?.SeedData ?? true)
-                    DbInitializer.Initialize(context);
-            
-            // Check for admin user
-            AddOrUpdateAdminUser(context, builder.Configuration.GetSection(AdminSettings.SECTION).Get<AdminSettings>());
-        }
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            app.Run();
+        }
+        catch (OptionsValidationException ex)
         {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            using var loggerFactory = LoggerFactory.Create(logging =>
+            {
+                logging.AddConsole();
+                logging.AddDebug();
+            });
+
+            var logger = loggerFactory.CreateLogger<Program>();
+
+            logger.LogError(ex, ex.Message);
+
+            Environment.Exit(1);
         }
+        catch (Exception ex)
+        {
+            using var loggerFactory = LoggerFactory.Create(logging =>
+            {
+                logging.AddConsole();
+                logging.AddDebug();
+            });
 
-        app.UseAuthentication();
-        app.UseAuthorization();
-        app.MapControllers();
+            var logger = loggerFactory.CreateLogger<Program>();
 
-        app.Run();
+            logger.LogError(ex, "An unexpected error occurred during startup.");
+            
+            Environment.Exit(1);
+        }
     }
 
     private static void AddOrUpdateAdminUser(MyMasternodeAuthDbContext database, AdminSettings? adminSettings)
